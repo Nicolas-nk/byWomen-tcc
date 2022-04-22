ae2ca15c9df966cd42707b38cf0b8c93d884118c
Nicolas-nk <nicolas0102messias@gmail.com>
2022-04-22 13:31:36 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('app/public'));
+
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+var rotas = require('./app/routes/router');
+app.use('/', rotas);
+
+app.listen(port, () => {
+    console.log(`Servidor ouvindo na porta: ${port}`)
+})
