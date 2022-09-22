024d4e4df1caf9294f3bbbd4a18302c68dd019f0
Nicolas-nk <nicolas0102messias@gmail.com>
2022-09-22 14:18:36 -0300

    deploy config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 var session = require("express-session");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('app/public'));
 
