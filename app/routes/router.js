1f83063a9b82b30d700bda8b3b5f83b957abc145
Nicolas-nk <nicolas0102messias@gmail.com>
2022-10-11 08:10:17 -0300

    rotas e profissões

--- app/routes/router.js
+++ app/routes/router.js
@@ -15,7 +15,7 @@ router.get("/", function (req, res) {
         }
       : { colaboradora_autenticado: null };
 
-  res.render("pages/home/index", req.session);
+  res.render("pages/home/index", {session: req.session});
 });
 
 router.get("/login", function (req, res) {
@@ -61,28 +61,62 @@ router.get("/perfil", function (req, res) {
   if (req.session.autenticado === true) {
     if (req.session.colaboradora_autenticado === true) {
       dbConnection.query(
-        "SELECT * FROM trabalhos_realizados WHERE id_colaboradora = ?",
+        "SELECT cod_profissao FROM profissao_colaboradora WHERE id_colaboradora = ?",
         [req.session.usu_colaboradora_autenticado_id],
         (error, results) => {
           if (error) {
             return reject(error);
           }
-          req.trabalhos_realizados = results[0] !== undefined ? results : null;
-
+          req.profissao_colaboradora = [];
+          if (results.length > 0) {
+            for (let i = 0; i < results.length; i++) {
+              req.profissao_colaboradora[i] = results[i].cod_profissao;
+            }
+          } else {
+            req.profissao_colaboradora = 0;
+          }
           setTimeout(function () {
             dbConnection.query(
-              "SELECT * FROM certificacao WHERE id_colaboradora = ?",
-              [req.session.usu_colaboradora_autenticado_id],
+              "SELECT * FROM profissao WHERE cod_profissao IN (?)",
+              [req.profissao_colaboradora],
               (error, results) => {
                 if (error) {
-                  return reject(error);
+                  console.log(error);
                 }
-                req.certificacoes = results[0] !== undefined ? results : null;
-                res.render("pages/perfilColaboradora/index", {
-                  session: req.session,
-                  trabalhos_realizados: req.trabalhos_realizados,
-                  certificacoes: req.certificacoes,
-                });
+                req.profissao_selecionada_colaboradora = results;
+                setTimeout(function () {
+                  dbConnection.query(
+                    "SELECT * FROM trabalhos_realizados WHERE id_colaboradora = ?",
+                    [req.session.usu_colaboradora_autenticado_id],
+                    (error, results) => {
+                      if (error) {
+                        return reject(error);
+                      }
+                      req.trabalhos_realizados =
+                        results[0] !== undefined ? results : null;
+                      setTimeout(function () {
+                        dbConnection.query(
+                          "SELECT * FROM certificacao WHERE id_colaboradora = ?",
+                          [req.session.usu_colaboradora_autenticado_id],
+                          (error, results) => {
+                            if (error) {
+                              return reject(error);
+                            }
+                            req.certificacoes =
+                              results[0] !== undefined ? results : null;
+                            res.render("pages/perfilColaboradora/index", {
+                              session: req.session,
+                              trabalhos_realizados: req.trabalhos_realizados,
+                              certificacoes: req.certificacoes,
+                              profissao_selecionada_colaboradora:
+                                req.profissao_selecionada_colaboradora,
+                            });
+                          }
+                        );
+                      }, 200);
+                    }
+                  );
+                }, 200);
               }
             );
           }, 200);
@@ -108,33 +142,69 @@ router.get("/crie-perfil-profissional", function (req, res) {
 
 router.get("/editarperfil", function (req, res) {
   dbConnection.query(
-    "SELECT * FROM trabalhos_realizados WHERE id_colaboradora = ?",
+    "SELECT cod_profissao FROM profissao_colaboradora WHERE id_colaboradora = ?",
     [req.session.usu_colaboradora_autenticado_id],
     (error, results) => {
       if (error) {
         return reject(error);
       }
-      req.trabalhos_realizados = results;
-
+      req.profissao_colaboradora = [];
+      if (results.length > 0) {
+        for (let i = 0; i < results.length; i++) {
+          req.profissao_colaboradora[i] = results[i].cod_profissao;
+        }
+      } else {
+        req.profissao_colaboradora = 0;
+      }
       setTimeout(function () {
         dbConnection.query(
-          "SELECT * FROM certificacao WHERE id_colaboradora = ?",
-          [req.session.usu_colaboradora_autenticado_id],
+          "SELECT * FROM profissao WHERE cod_profissao IN (?)",
+          [req.profissao_colaboradora],
           (error, results) => {
             if (error) {
-              return reject(error);
+              console.log(error);
             }
-            req.certificacoes = results;
+            req.profissao_selecionada_colaboradora = results;
+            setTimeout(function () {
+              dbConnection.query(
+                "SELECT * FROM trabalhos_realizados WHERE id_colaboradora = ?",
+                [req.session.usu_colaboradora_autenticado_id],
+                (error, results) => {
+                  if (error) {
+                    return reject(error);
+                  }
+                  req.trabalhos_realizados = results;
 
-            if (req.session.autenticado === true) {
-              res.render("pages/formColaboradora/criePerfil/index", {
-                session: req.session,
-                trabalhos_realizados: req.trabalhos_realizados,
-                certificacoes: req.certificacoes,
-              });
-            } else {
-              res.redirect("/login");
-            }
+                  setTimeout(function () {
+                    dbConnection.query(
+                      "SELECT * FROM certificacao WHERE id_colaboradora = ?",
+                      [req.session.usu_colaboradora_autenticado_id],
+                      (error, results) => {
+                        if (error) {
+                          return reject(error);
+                        }
+                        req.certificacoes = results;
+
+                        if (req.session.autenticado === true) {
+                          res.render(
+                            "pages/formColaboradora/criePerfil/index",
+                            {
+                              session: req.session,
+                              trabalhos_realizados: req.trabalhos_realizados,
+                              certificacoes: req.certificacoes,
+                              profissao_selecionada_colaboradora:
+                                req.profissao_selecionada_colaboradora,
+                            }
+                          );
+                        } else {
+                          res.redirect("/login");
+                        }
+                      }
+                    );
+                  }, 200);
+                }
+              );
+            }, 200);
           }
         );
       }, 200);
@@ -144,18 +214,18 @@ router.get("/editarperfil", function (req, res) {
 
 router.get("/configuracao", function (req, res) {
   if (req.session.autenticado === true) {
-    res.render("pages/configuracao/index", req.session);
+    res.render("pages/configuracao/index", {session: req.session});
   } else {
     res.redirect("/login");
   }
 });
 
 router.get("/perfilColaboradora", function (req, res) {
-  res.render("pages/perfilColaboradora-visãoCliente/index", req.session);
+  res.render("pages/perfilColaboradora-visãoCliente/index", {session: req.session});
 });
 router.get("/trabalho-realizado", function (req, res) {
   if (req.session.autenticado === true) {
-    res.render("pages/formColaboradora/trabalhoRealizado/index", req.session);
+    res.render("pages/formColaboradora/trabalhoRealizado/index", {session: req.session});
   } else {
     res.redirect("/login");
   }
@@ -188,7 +258,7 @@ router.get("/trabalho-realizado/:id", function (req, res) {
 });
 router.get("/certificacao", function (req, res) {
   if (req.session.autenticado === true) {
-    res.render("pages/formColaboradora/certificacao/index", req.session);
+    res.render("pages/formColaboradora/certificacao/index", {session: req.session});
   } else {
     res.redirect("/login");
   }
@@ -274,18 +344,18 @@ router.get("/profissao", function (req, res) {
   }
 });
 router.get("/historico", function (req, res) {
-  res.render("pages/historico/index", req.session);
+  res.render("pages/historico/index", {session: req.session});
 });
 router.get("/pedidos", function (req, res) {
-  res.render("pages/pedidos/index", req.session);
+  res.render("pages/pedidos/index", {session: req.session});
 });
 
 router.get("/planos", function (req, res) {
-  res.render("pages/planos/index", req.session);
+  res.render("pages/planos/index", {session: req.session});
 });
 
 router.get("/servicos", function (req, res) {
-  res.render("pages/servicos/index", req.session);
+  res.render("pages/servicos/index", {session: req.session});
 });
 
 router.get("/recuperar-senha", function (req, res) {
@@ -340,42 +410,7 @@ router.get("/Construcao", function (req, res) {
     req.session.autenticado === true
       ? { autenticado: req.session.usu_autenticado_id }
       : { autenticado: null };
-  res.render("pages/subcategorias-servicos/construção/index", req.session);
-});
-
-router.get("/Reparos", function (req, res) {
-  autenticado =
-    req.session.autenticado === true
-      ? { autenticado: req.session.usu_autenticado_id }
-      : { autenticado: null };
-  res.render("pages/subcategorias-servicos/reparos/index", req.session);
-});
-
-router.get("/Projetos", function (req, res) {
-  autenticado =
-    req.session.autenticado === true
-      ? { autenticado: req.session.usu_autenticado_id }
-      : { autenticado: null };
-  res.render("pages/subcategorias-servicos/projetos/index", req.session);
-});
-
-router.get("/Montagens", function (req, res) {
-  autenticado =
-    req.session.autenticado === true
-      ? { autenticado: req.session.usu_autenticado_id }
-      : { autenticado: null };
-  res.render("pages/subcategorias-servicos/montagem/index", req.session);
-});
-
-router.get("/Manutencao-eletrica", function (req, res) {
-  autenticado =
-    req.session.autenticado === true
-      ? { autenticado: req.session.usu_autenticado_id }
-      : { autenticado: null };
-  res.render(
-    "pages/subcategorias-servicos/manutencao-eletrica/index",
-    req.session
-  );
+  res.render("pages/categorias_profissoes/index", req.session);
 });
 
 module.exports = router;
